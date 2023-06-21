2552d316e42d46d657a3dff79275a2049a39425b
Bastian Bloessl <mail@bastibl.net>
2023-06-21 14:22:11 +0200

    petalinux for new hw design

--- futuresdr/boot.sh
+++ futuresdr/boot.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-petalinux-boot --jtag --kernel --fpga --bitstream project-spec/hw-description/mpsoc_preset_wrapper.bit  -v
+petalinux-boot --jtag --kernel --fpga --bitstream project-spec/hw-description/receiver8_atan.bit  -v
